1427f8d505e4fa2f7a20e49e87992b9f73786fd6
Tao <zxc5540599@gmail.com>
2017-12-20 17:53:46 +0800

    generate wrong matches

--- gen_tfrecord.py
+++ gen_tfrecord.py
@@ -25,18 +25,25 @@ def _bytes_feature(value):
 def convert_to(X, name):
     imgs = X['image']
     caps = X['caption']
-
+    imgs_w = X['image_wrong']
+    caps_w = X['caption_wrong']
+    
     filename = os.path.join(DIR_DATA, name + '.tfrecords')
     with tf.python_io.TFRecordWriter(filename) as writer:
         for idx in range(len(X)):
             image_raw = imgs[idx].tostring()
+            image_raw_w = imgs_w[idx].tostring()
             example = tf.train.Example(
-                features=tf.train.Features(
-                    feature={
-                        'image': _bytes_feature(tf.compat.as_bytes(image_raw)),
-                        'caption': _int64_feature(caps[idx], pass_list=True)
-                    }))
+            features=tf.train.Features(
+                feature={
+                    'image': _bytes_feature(tf.compat.as_bytes(image_raw)), 
+                    'caption': _int64_feature(caps[idx], pass_list=True),
+                    'image_wrong': _bytes_feature(tf.compat.as_bytes(image_raw_w)), 
+                    'caption_wrong': _int64_feature(caps_w[idx], pass_list=True)
+                }))
             writer.write(example.SerializeToString())
+    
+
 
 if __name__ == '__main__':
     df = pd.read_pickle(DIR_DATA + 'text2ImgData.pkl')
@@ -55,14 +62,19 @@ if __name__ == '__main__':
         im = np.asarray(im)
         for cap in caps:
             cap = [int(x) for x in cap]
-            examples.append([im, cap])
+            im_w = Image.open(DIR_DATA + df['ImagePath'].iloc[np.random.randint(len(df))]).resize((500, 500))
+            im_w = np.asarray(im_w)
+            cap_w_candidates = df['Captions'].iloc[np.random.randint(len(df))]
+            cap_w = cap_w_candidates[np.random.randint(len(cap_w_candidates))]
+            cap_w = [int(x) for x in cap_w]
+            examples.append([im, cap, im_w, cap_w])
             cnt += 1
-            if cnt % 10000 == 0:
-                print('Finished: %d, time: %4.4fs' %
-                      (cnt, time.time() - st_time))
+            if cnt % 1000 == 0:
+                print('finished: %d, time: %4.4fs' % (cnt, time.time() - st_time))
 
-    X = pd.DataFrame(examples, columns=['image', 'caption'])
 
-    convert_to(X, 'img_cap_pairs')
+    X = pd.DataFrame(examples, columns=['image', 'caption', 'image_wrong', 'caption_wrong'])
+
+    convert_to(X, 'train_data')
     print('Create %s, total Time: %4.4fs' %
-          (DIR_DATA + 'img_cap_pairs', time.time() - st_time))
+          (DIR_DATA + 'train_data', time.time() - st_time))
